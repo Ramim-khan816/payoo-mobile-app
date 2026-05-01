5d25cb9f449b6716f927fe017934e62f458502fd
Ramim Khan <ramimkhan816@gmail.com>
2026-05-01 09:03:32 +0600

    first commit

---
+++ project/script.js
@@ -0,0 +1,25 @@
+document.getElementById('loginbtn').addEventListener('click', function(e){
+    e.preventDefault()
+    console.log("login successfully")
+    const mobileNumber = 1234567890
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById("mobile-number").value
+
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+     
+
+    const pinNumberValue =document.getElementById("pin-num").value
+
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNumberValueConverted, pinNumberValueConverted)
+    
+
+// if (mobileNumber length > 10){
+//         console.log("valid number")
+        
+    
+//     }
+//     alert("invalid credintials")
+})
