e765b9d84d59ca1fca209ae133dc0d47e9d80bc5
Ramim Khan <ramimkhan816@gmail.com>
2026-05-15 11:23:39 +0600

    last commit

--- project/home.js
+++ project/home.js
@@ -25,6 +25,16 @@ function setInnerText(value) {
   avilableBalanceElement.innerText = value
 }
 
+function HandleToggle(id){
+  const forms = document.getElementsByClassName("form")
+  for(const form of forms){
+    form.style.display = "none"
+  }
+  toggleVlaue(id).style.display="block"
+}
+
+const transationData = []
+
 const mainAccountNumber = 1234567890
 const mainAgentNumber = 1919764171
 const pin = 12345
@@ -61,9 +71,12 @@ document.getElementById("add-money-btn").addEventListener("click",function(e) {
 
       setInnerText(newAvailableBalance)
        
-      
+    const data = {
+      name: "Add Money",
+      date: new Date().toLocaleTimeString()
+    }  
      
-     
+     transationData.push(data)
 })
 
 
@@ -91,19 +104,96 @@ else{
   const newRemainingBalance = avilableBalance - withdrawMoney
 
    setInnerText(newRemainingBalance)
+      
+   const data = {
+      name: "Cash Out",
+      date: new Date().toLocaleTimeString()
+    }  
+     
+     transationData.push(data)
+
+})
 
 
+document.getElementById("transactions-button").addEventListener("click", function () {
+   
+  const transactionCont = document.getElementById("transactions-cont")
+  transactionCont.innerText = ""
+  for(const data of transationData){
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class=" flex bg-white justify-between items-center rounded-2xl mt-2">
+                     <div class="flex items-center rounded-2xl p-4">
+                     <div class="p-2 m-2 rounded-4xl bg-[#f4f5f7]">
+                        <img src="./assets/wallet1.png" alt="">
+                     </div>
+
+                    <div class="">
+                        <h1 class="font-semibold">${data.name}</h1>
+                        <p>${data.date}</p>
+                    </div>
+
+                     
+                </div>
+
+                   <i class="fa-solid fa-ellipsis p-4"></i>
+
+                </div>
+    `
+
+    transactionCont.appendChild(div)
+  }
+
+
+  
 })
 
 
 document.getElementById("add-button").addEventListener("click", function(){
-  toggleVlaue("add-money-perants").style.display="block"
+   HandleToggle("add-money-perants")
 
-  toggleVlaue("cashout-perants").style.display= "none"
-})
+
+
+   const formBtns = document.getElementsByClassName("from-btn")
+
+   for(const btns of formBtns){
+    btns.classList.remove("border-[#0874f2]", "bg-[#0874f20d")
+    btns.classList.add("border-gray-300")
+   }
+
+  document.getElementById("add-button").classList.remove("border-gray-300")
+  document.getElementById("add-button").classList.add("border-[#0874f2]", "bg-[#0874f20d")
+});
 
 document.getElementById("cash-out-button").addEventListener("click", function(){
-  toggleVlaue("cashout-perants").style.display="block"
-
-  toggleVlaue("add-money-perants").style.display= "none"
+   HandleToggle("cashout-perants")
+    
 })
+
+document.getElementById("transfer-money-button").addEventListener("click", function(){
+   HandleToggle("Transfer-money-perants")
+
+   const data = {
+      name: "Cash Out",
+      date: new Date().toLocaleTimeString()
+    }  
+     
+     transationData.push(data)
+})
+
+document.getElementById("get-bonus-button").addEventListener("click", function(){
+   HandleToggle("get-bonus-perants")
+   
+})
+
+document.getElementById("pay-bill-button").addEventListener("click", function(){
+   HandleToggle("pay-bill-perants")
+})
+
+document.getElementById("transactions-button").addEventListener("click", function(){
+  
+   HandleToggle("transaction-perants")
+})
+
+
+ 
